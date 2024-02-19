eabae6c8a556a531f86fab89c604a8cd79db4128
talhaonder <ltalhaonderl@gmail.com>
2024-02-19 18:51:37 +0300

    others

---
+++ src/components/customApp.tsx
@@ -0,0 +1,25 @@
+import {Animated} from 'react-native';
+
+export const AnimatedAppBar = (translateY: any, navigation: any) => {
+  return (
+    <Animated.View
+      style={{
+        flexDirection: 'row',
+        justifyContent: 'space-around',
+        // backgroundColor: 'rgba(52, 52, 52, 0)',
+        backgroundColor: 'red',
+        width: '100%',
+
+        //for animation
+        height: 64,
+        transform: [{translateY: translateY}],
+        position: 'absolute',
+        top: 0,
+        right: 0,
+        left: 0,
+        elevation: 4,
+        zIndex: 1,
+      }}>
+    </Animated.View>
+  );
+};
